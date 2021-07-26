539c5e923558a5d99ed8bf16d0d889aa5f74d608
jangulo-rb <jangulo@redb.ee>
2021-07-26 09:27:03 -0300

    addind content

--- test/TDD/tdd.scala
+++ test/TDD/tdd.scala
@@ -7,16 +7,44 @@ class tdd extends PlaySpec {
   "TusLibros system must have" must {
 
     "Start shopping with an empty car" in {
-      val shoppingCar = ShoppingCar()
-      shoppingCar.isEmpty() mustBe true
+      val aShoppingCar = ShoppingCar()
+      aShoppingCar.isEmpty() mustBe true
     }
 
     "Add a new item and the car contain that" in {
-      val shoppingCar = ShoppingCar()
-      val item: IProduct = Book(name = "Harry Potter")
-      shoppingCar.add(item)
-      shoppingCar.contain(item) mustBe true
+      val aShoppingCar = ShoppingCar()
+      val aItem: IProduct = Book(name = "Harry Potter")
+      aShoppingCar.add(aItem)
+      aShoppingCar.contain(aItem) mustBe true
     }
+
+    "Add two items and the car contain that" in {
+      val aShoppingCar = ShoppingCar()
+      val aItem: IProduct = Book(name = "Harry Potter")
+      val aItem2: IProduct = Book(name = "Señor de los Anillos")
+      aShoppingCar.add(aItem)
+      aShoppingCar.add(aItem2)
+      aShoppingCar.contain(aItem) mustBe true
+      aShoppingCar.contain(aItem2) mustBe true
+    }
+
+    /*
+
+    los test que segun hay
+    1. Comienzo shopping con carrito vacío
+2. Agrego un libro y el carrito lo contiene
+3. Agrego 2 o más libros diferentes y los contiene
+4. Agrego más de 1 ejemplar al mismo y los contiene
+5. Agrego un libro más de una vez y los contiene
+6. No puedo agregar libros que no pertenecen a la editorial
+7. Sólo puedo agregar cantidades estrictamente positivas de libros
+
+
+
+     */
+
+
+
 
   }
 }
